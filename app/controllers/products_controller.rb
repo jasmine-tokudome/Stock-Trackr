79f85c2909a1956df8e227c660bca9f784440116
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-04-27 02:45:15 +0900

    エラー対処

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,12 @@
 require 'net/http'
+require 'uri'
 require 'json'
 
 class ProductsController < ApplicationController
 
   def edit
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-    @products = Product.all
-    end
-    # if response.code == '200'
-    #   json = JSON.parse(response.body)
-    #   # 取得したJSONデータを処理する
-    # else
-    #   response.code == '404100'
-    #   # エラー処理
-    # end
+    Product.set_product_data
+    @product = Product.new
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,13 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-class MyModel < ApplicationRecord
-  def self.get_api_data()
-    url = 'https://api.shop-pro.jp'
+class Product < ApplicationRecord
+
+  def self.get_api_data
+    url = 'https://api.shop-pro.jp/v1/shop.json'
     uri = URI(url)
     request = Net::HTTP::Get.new(uri)
-    request['Authorization'] = "Bearer #{ENV['colorme_access_token']}"
+    request['Authorization'] = "Bearer ENV[colorme_access_token]"
     request['Content-Type'] = 'application/json'
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
@@ -16,4 +17,10 @@ class MyModel < ApplicationRecord
     # 取得したデータをハッシュに変換して返す
     data.to_h
   end
+
+  def self.set_product_data
+    @product = self.get_api_data
+  end
 end
+
+
