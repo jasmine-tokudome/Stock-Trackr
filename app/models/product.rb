c44f8bdd0efdb7aee0a16488bdc6a5b47f7c7943
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-04-28 02:28:07 +0900

    scopesの記述修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,11 +6,11 @@ class Product < ApplicationRecord
 
   def self.get_api_data
     url = 'https://api.shop-pro.jp/v1/shop.json'
-    scopes = 'read_products'
     uri = URI(url)
     request = Net::HTTP::Get.new(uri)
     request['Authorization'] = "Bearer ENV[colorme_access_token]"
     request['Content-Type'] = 'application/json'
+    request['scopes'] = 'read_products'
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
     end
