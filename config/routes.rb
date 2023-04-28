e86c148843009bf5e3d7e4134b43a4767e8131a6
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-04-28 16:55:15 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'password_resets/new'
   get 'password_resets/edit'
-  root "static_pages#home"
   get  "/help",    to: "static_pages#help"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  get '/products/edit', to: 'products#edit'
+  get '/products', to: 'products#edit'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :products
+  resources :products, only: [:edit]
 end
