a644fb4347915c918337d3a004d755f7d3fcc11a
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-05-12 00:56:02 +0900

    putリクエストの挙動を調べる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,12 +13,12 @@ class ProductsController < ApplicationController
 
   def update_stocks
   # products_stocks_paramsを使って、更新処理を実行する
-  products_stocks_params = params.require(:products_stocks).permit!
+  products_stocks_params = params.require(:product).permit(stocks: {})
   # 外部APIにPUTリクエストを送信するロジックを実装する
-  products_stock_details = params[:products_stocks][:products_stock_details]
-  products_stock_details.each do |id, details|
+  products_stock_details = products_stocks_params[:stocks]
+  products_stock_details.each do |id,stocks|
     # PUTリクエストを送信するコードを記述する
-    url = "https://api.shop-pro.jp/v1/products/#{id}"
+    url = "https://api.shop-pro.jp/v1/products/#{id}.json"
     uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
     # http.use_ssl = true
@@ -29,18 +29,15 @@ class ProductsController < ApplicationController
     request['X-CSRF-Token'] = csrf_token
     request['Content-Type'] = 'application/json'
     request['scopes'] = 'write_products'
-    request.body = { id: "id", stocks: "stocks" }.to_json
+    request.body = { product: { stocks: params[:product][:stocks] } }.to_json
     response = http.request(request)
     if response.present? && response.code.to_i == 200
-    redirect_to "/products/edit", notice: '更新が完了しました'
+      next
     else
-    redirect_to "/products/edit", flash: { alert: '更新に失敗しました。再度修正してください' }
+      render status: :bad_request, json: { message: '更新に失敗しました。再度修正してください' }
+      return
     end
   end
+  head :no_content
 end
-
-# @products_stocks
-# {174335511=>{"id"=>174335511, "name"=>"商品D", "stock_managed"=>false, "stocks"=>nil}, 174335508=>{"id"=>174335508, "name"=>"商品C", "stock_managed"=>false, "stocks"=>nil}, 174335506=>{"id"=>174335506, "name"=>"商品B", "stock_managed"=>true, "stocks"=>0}, 174335504=>{"id"=>174335504, "name"=>"商品A", "stock_managed"=>true, "stocks"=>100}}
-
-
 end
