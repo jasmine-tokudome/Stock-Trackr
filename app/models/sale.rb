0201ddc2a787ba92f4da0a2468a23948f24f663f
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-06-04 21:43:21 +0900

    売上分析にどのデータを抽出するか検討中

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,25 @@
-class Sale < ApplicationRecord
+require 'net/http'
+require 'uri'
+require 'json'
+
+class Sale
+    
+      def self.get_sales_date
+        #  受注情報のうち必要な3項目のみを取り出す（売上ID、受注日時、商品の合計金額、入金済みであるか否か、受注内容詳細）
+         url = "https://api.shop-pro.jp/v1/sales.json?fields=id,make_date,paid,product_total_price,details"
+         uri = URI(url)
+         request = Net::HTTP::Get.new(uri)
+         request['Authorization'] = "Bearer #{ENV['colorme_access_token']}"
+         request['Content-Type'] = 'application/json'
+         request['scopes'] = 'read_products'
+         response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+           http.request(request)
+         end
+         json_data = JSON.parse(response.body)
+         sale = json_data["sale"]
+         sales[sales_id] = sales
+         end
+         return sales
+       end
+
 end
